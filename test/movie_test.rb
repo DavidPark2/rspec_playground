90d06eecee7f78e1fbea6f99ddebb44a7eef9719
David Park <dpark543107@gmail.com>
2016-05-02 09:49:21 -0500

    got some test to work

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -8,4 +8,11 @@ describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
   end
+
+  it "should be Top Gun" do
+    @tg = Movie.new.to_hash
+    @tg["Title"].should == "Top Gun"
+    @tg["Year"].should == "1986"
+    @tg["Rated"].should == "PG"
+  end
 end
